47381670f2bd5a1611e9fa261c79c199b8826028
Galirus_Official™ <114099161+Galirus404@users.noreply.github.com>
2023-12-24 21:16:12 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ p="\033[1;37m"
 while [ "$else" = "$ulang" ]
 do
 clear
+echo $q $bl "[$m SUBSCRIBE$bl ]$b>$k dulu Channel Saya$bl :)"
+sleep 3
+$url "https://www.youtube.com/@GalirusOfficial"
+sleep 2
+clear
+clear
 echo $q $m"╔══╗        ╔╗      ╔══╗"
 echo $q "║╔╗║        ║║      ║╔╗║"
 echo $q "║╚╝║╔══╗╔══╗║║╔╗╔══╗║╚╝║╔══╗"
@@ -35,60 +41,55 @@ if [ "$apa" = "1" ]
 then
 echo $q $k "menginstall Package"
 apt update && apt upgrade -y
-apt install nala -y
-nala install git bash python python2 python3 ruby curl wget zip unzip figlet openssh which cloudflared figlet cowsay  mpv -y
+apt install git bash python python2 python3 ruby curl -y
+apt wget zip unzip figlet openssh which cloudflared figlet cowsay sox -y
 pip install requests mechanize bs4
 pip install pycryptodome keyboard rich colorama tqdm
 pip install -r requirements.txt
 git clone https://github.com/Galirus404/TOOLSV3
-cd TOOLSV3
 clear
+git pull origin main
 echo $q $k
 echo -ne "	installasi sukses\r"
 sleep 2
 clear
 echo -ne "	Membuka TOOLSV4"
 sleep 2
-cd
-cd TOOLSV4
 chmod 777 README.sh
 clear
-./README.sh
+bash README.sh  
 elif [ "$apa" = "2" ]
 then
 echo $q $k "menginstall Package"
 sudo apt update
-sudo apt install nala
-sudo nala install git bash python python2 python3 ruby curl wget zip unzip figlet openssh which cloudflared figlet cowsay  mpv -y
+sudo apt install git bash  python2 python3 ruby curl -y
+sudo apt wget zip unzip figlet openssh which cloudflared figlet cowsay sox -y
 sudo pip install requests mechanize bs4
-sudo pip install pycryptodome keyboard rich colorama tqdm
+sudo pip install pycryptodome keyboard rich colorama tqdm Crypto
 pip install -r requirements.txt
 git clone https://github.com/Galirus404/TOOLSV3
-cd TOOLSV3
-clear
+git pull origin main
 echo $q $k
 echo -ne "	installasi sukses\r"
 sleep 2
 clear
 echo -ne "	Membuka TOOLSV4"
 sleep 2
-cd
-cd TOOLSV4
 chmod 777 README.sh
 clear
-./README.sh
+bash README.sh  
 elif [ "$apa" = "n" ]
 then
+git pull origin main
 echo $q $k
 echo -ne "	Melewati Instalasi\r"
 sleep 2
 clear
 echo -ne "	Membuka TOOLSV4"
 sleep 2
-cd TOOLSV4
 chmod 777 README.sh
 clear
-./README.sh
+bash README.sh  
 elif [ "$apa" = "0" ]
 then
 echo $q $k "Jangan Lupa$m SUBSCRIBE$b Galirus Official"
